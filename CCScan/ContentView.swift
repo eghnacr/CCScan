9e5a4dd419ccccc8b7300a1688b3eb12ef58e373
eghnacr <eghnacr@gmail.com>
2022-03-14 14:49:12 +0300

    add camera ui

--- CCScan/ContentView.swift
+++ CCScan/ContentView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            ccScannerView()
+            ZStack {
+                GeometryReader { proxy in
+                    let w = proxy.size.width * 0.9
+                    let h = 53.98 * (w / 85.6)
+                    let x = proxy.size.width / 2
+                    let y = (h / 2) + (proxy.size.height * 0.05)
+                    Rectangle()
+                        .fill(.black)
+                        .frame(width: proxy.size.width,
+                               height: proxy.size.height)
+                        .opacity(0.7)
+                    Rectangle()
+                        .frame(width: w, height: h)
+                        .cornerRadius(15)
+                        .blendMode(.destinationOut)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(.red, lineWidth: 5)
+                        )
+                        .position(x: x, y: y)
+                }
+            }.compositingGroup()
+        }.ignoresSafeArea()
+    }
+
+    private func ccScannerView() -> some View {
+        #if targetEnvironment(simulator)
+        return Image("camera.place.holder")
+            .resizable()
+        #else
+        return CCScannerView()
+        #endif
     }
 }
 
