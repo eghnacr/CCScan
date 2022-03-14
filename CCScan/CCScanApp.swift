f27e6e85c92084ab956bf2a6562f2cdc014dde15
eghnacr <eghnacr@gmail.com>
2022-03-14 13:52:25 +0300

    Initial Commit

---
+++ CCScan/CCScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCScanApp.swift
+//  CCScan
+//
+//  Created by Egehan Acar on 14.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct CCScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
